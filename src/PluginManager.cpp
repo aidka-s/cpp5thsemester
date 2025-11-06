072ad8ca06f9c4d5ad65e2c88e7ada6e02e79c6e
Гиниятуллина Аида <hellafree4@gmail.com>
2025-11-06 23:43:14 +0300

    feat: implement dynamic DLL loading using LoadLibrary

--- src/PluginManager.cpp
+++ src/PluginManager.cpp
@@ -1,8 +1,33 @@
 #include "PluginManager.h"
 #include <iostream>
+#include <filesystem>
+#include <windows.h>
+namespace fs = std::filesystem;
 
 void PluginManager::loadPlugins(const std::string& folder) {
-    std::cout << "Loading plugins from " << folder << "..." << std::endl;
-    // пока просто выводим сообщение, без загрузки
+    std::cout << "Loading plugins from " << folder << "...\n";
+
+    if (!fs::exists(folder)) {
+        std::cout << "No plugin folder found.\n";
+        return;
+    }
+
+    for (auto& entry : fs::directory_iterator(folder)) {
+        if (entry.path().extension() == ".dll") {
+            auto path = entry.path().string();
+            HMODULE dll = LoadLibraryA(path.c_str());
+
+            if (!dll) {
+                std::cerr << "Failed to load " << path << std::endl;
+                continue;
+            }
+
+            dlls.push_back(dll);
+            std::cout << "Loaded: " << entry.path().filename().string() << std::endl;
+        }
+    }
+
+    if (dlls.empty())
+        std::cout << "No plugins loaded.\n";
 }
 
